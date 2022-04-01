6e59926cf19dd78b0fba5ac4f6af3c9fef6daa54
shiba <shiba307@icloud.com>
2022-04-01 18:46:53 +0900

    TicketBuyResultがステージングされてなかったので

---
+++ src/main/java/org/docksidestage/bizfw/basic/buyticket/TicketBuyResult.java
@@ -0,0 +1,32 @@
+package org.docksidestage.bizfw.basic.buyticket;
+
+/**
+ * @author shibata shungo
+ */
+public class TicketBuyResult {
+    // Ticket change
+    // ===================================================================================
+    //                                                                           Attribute
+    //                                                                           =========
+    private final int change;
+    private Ticket ticket;
+
+    // ===================================================================================
+    //                                                                         Constructor
+    //                                                                         ===========
+    public TicketBuyResult(int change, int ticketPrice) {
+        this.change = change;
+        ticket = new Ticket(ticketPrice);
+    }
+
+    // ===================================================================================
+    //                                                                            Accessor
+    //                                                                            ========
+    public int getChange() {
+        return change;
+    }
+
+    public Ticket getTicket() {
+        return ticket;
+    }
+}
