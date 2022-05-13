bf4e384e2e320ef190937bb5fa8cc899ed2ec9e0
shiba <shiba307@icloud.com>
2022-05-13 20:18:23 +0900

    TicketQuantity微修正

--- src/main/java/org/docksidestage/bizfw/basic/buyticket/TicketQuantity.java
+++ src/main/java/org/docksidestage/bizfw/basic/buyticket/TicketQuantity.java
@@ -4,12 +4,21 @@ package org.docksidestage.bizfw.basic.buyticket;
  * @author Shibata shungo 
  */
 public class TicketQuantity {
+    // ===================================================================================
+    //                                                                           Attribute
+    //                                                                           =========
     private int quantity;
 
+    // ===================================================================================
+    //                                                                         Constructor
+    //                                                                         ===========
     public TicketQuantity(int ticketQuantity) {
         this.quantity = ticketQuantity;
     }
 
+    // ===================================================================================
+    //                                                                            Accessor
+    //                                                                            ========
     public int getQuantity() {
         return quantity;
     }
